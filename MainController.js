cdd6dc6f32b2cee851eb56349ecb8be9c8834470
strahlistvan <strahlistvan@users.noreply.github.com>
2016-05-14 22:49:46 +0200

    Small CSS custumizations

--- MainController.js
+++ MainController.js
@@ -1,10 +1,10 @@
 app.controller('MainController', ['$scope', '$http', function($scope, $http) { 
   $scope.title = 'Find your favorite movie here!'; 
   $scope.promo = 'Write something in the textbox.';
-  $scope.data = [ {"Title": "Semmise"} ];
-  $scope.response = "";
+  $scope.data = [ ];
+  $scope.response = "";  //just for debugging...
   
-  $scope.change = function(searchKey)
+  $scope.changeInput = function()
   {
       var baseURL = "http://www.omdbapi.com/?plot=full&r=json";
       var fullURL = baseURL+"&t="+$scope.searchKey + "&s="+$scope.searchKey;
@@ -14,6 +14,8 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 			     $scope.response = JSON.stringify(response);
 				 if (response.hasOwnProperty("Search"))
 					$scope.data = response.Search;
+				 else 
+				     $scope.data = [ ];
 			
 	             return response;
 		    })
@@ -21,7 +23,20 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 			     console.log("Error: "+err);
 			     return err;
 			});
+			
+	    //not really working
+		console.log("megvan!"+JSON.stringify($scope.data));
+	    for (var elem in $scope.data)
+		{
+		  var baseURL = "http://www.omdbapi.com/?plot=full&tomatoes=true&r=json";
+		  var fullURL = baseURL+"&i="+elem.imdbID + "&s="+$scope.searchKey;
+		  $http.get(fullURL).then(function(response) {
+				elem = response;
+				console.log(JSON.stringify(elem));
+		  });
+		}
   }
-	 
-    
+  
+ 
+  
 }]);
